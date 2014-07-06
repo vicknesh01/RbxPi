83c78270c667bf4eb8f45bc6eaa214c3c6c2e74c
Vickneshwaran Elangkeeran <vickneshwaran.g15@gmail.com>
2014-07-07 03:59:09 +0800

    reworked the grid algorithym, it now draws based on input size :)

--- detector.py
+++ detector.py
@@ -1,44 +1,93 @@
 import cv2
+import cv2.cv
 import numpy as np
 import time
 
 cap = cv2.VideoCapture(0)
-cap.set(3,300)
-cap.set(4,300)
+#override input resolution here
+cap.set(3,240) #height?
+cap.set(4,160) #width?
+
+#get stream size and other info
+print "INPUT VIDEO WIDTH:",cap.get(3)
+print "INPUT VIDEO HEIGHT:",cap.get(4)
+
+input_width = cap.get(3)
+input_height = cap.get(4)
+
+
+
+
+
+
+# a function to draw the grids on the video capture
+
+def calculateGuidelineGrid():
+	#this will calculate the coordinates for the gridlines
+	global x1,x2,x3,x4,x5,x6,x7,x8,y1,y2,y3,y4,y5,y6,y7,y8,widthInc,heightInc
+	widthInc= input_width/3
+	heightInc= input_height/3
+	x1=widthInc
+	y1=0
+	x2=widthInc*2
+	y2=0
+	x3=input_width
+	y3=heightInc
+	x4=input_width
+	y4=heightInc*2
+	x5=x2
+	y5=input_height
+	x6=x1
+	y6=input_height
+	x7=0
+	y7=y4
+	x8=0
+	y8=y3	
+	
+
+
+def drawGuidelineGrid():
+#draws the actual grid on the capture WARNING!!: MUST RUN calculateGuidelineGrid First
+	
+	cv2.line(frame,(int(x1),int(y1)),(int(x6),int(y6)),(0,255,255),2,8,0)
+	cv2.line(frame,(int(x2),int(y2)),(int(x5),int(y5)),(0,255,255),2,8,0)
+	cv2.line(frame,(int(x8),int(y8)),(int(x3),int(y3)),(0,255,255),2,8,0)
+	cv2.line(frame,(int(x7),int(y7)),(int(x4),int(y4)),(0,255,255),2,8,0)
+
+
+
+
 while(1):
 
-    # Take each frame
-    _, frame = cap.read()
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+# Take each frame
+	_, frame = cap.read()
+	
+#draw grid for basic rubix layout
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    #draw grid for basic rubix layout
-    #the output image has a size of w=176 and h=144
     
-    cv2.line(frame,(58,0),(58,144),(0,255,255),4,8,0)
-    cv2.line(frame,(117,0),(117,144),(0,255,255),4,8,0)
-    cv2.line(frame,(0,48),(176,48),(0,255,255),4,8,0)
-    cv2.line(frame,(0,96),(176,96),(0,255,255),4,8,0)
-  
+    
+	calculateGuidelineGrid()
+	drawGuidelineGrid()
+
+    
+
+    
+	
+
+	cv2.imshow('frame',frame)
+    
+    
+    
+	
 
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    time.sleep(1)
-    if k == 27:
-        break
+	k = cv2.waitKey(5) & 0xFF
+	#time.sleep(1)
+	if k == 27:
+		break
 
 cv2.destroyAllWindows()
